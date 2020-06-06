8afaf755682ab4a86c6f9d80b1316e3796cd3d24
Sabithapol <Sabitha.poladi10@gmail.com>
2020-06-06 00:11:53 -0700

    sixth

--- boxit.js
+++ boxit.js
@@ -1,4 +1,5 @@
-
+//1. should i use a size so the box doesnot change with the length of name
+//2. can i use arrays 
 // function to draw line
     function drawLine(a){
     let line ="━"
@@ -46,3 +47,41 @@ function drawmiddleline(a){
 }
     console.log(drawmiddleline("sabitha"));
 
+//function to draw bottomborder
+function drawBottomborder(a){
+    let start="┗"
+    let end="┛"
+    let string=""
+    for ( let i = 1; i <=a.length; i++){
+        if(i==1){string=string+start;
+        }else if(i==a.length){
+            string=string+end;
+        }else{
+            string=string+drawLine("sabitha")
+        }
+    return string;
+    }
+}
+    console.log(drawBottomborder("sabitha"));
+
+// function for bars around 
+function barsaround(a){
+    let bar="┃"
+    let string =""
+    for(i=1; i<=a.length; i++){
+        if(i==1){
+            string=string+bar
+        }else if(i==a.length){
+            string=string+bar
+        }else{
+            string=string+" ";
+        }
+    return string;
+    }
+}
+console.log(barsaround("sabitha"))
+//otherway for bars around
+if(NodeIterator=1){
+
+
+}
