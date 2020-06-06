5d9f81a9f6829e85be1f2b31285e45451bed93fd
Sabithapol <Sabitha.poladi10@gmail.com>
2020-06-05 22:21:46 -0700

    commit3

--- boxit.js
+++ boxit.js
@@ -20,13 +20,23 @@ drawline("sabitha")*/
      console.log(drawLine(19));
 
 // function to draw topborder
- function drawtopborder(b){
-     let topb ="┌"
-     let string1 = ""
-     for ( let i = 1; i <=a; i++){
-        string += line 
-       }
-       return string;
+function drawtopborder(b){
+    let topb ="┏"
+    let topb1="┓"
+    let string1 = ""
+    for ( let i = 1; i <=b; i++){
+        if(i==1){
+            string1=string1+topb;
+        }else if(i==19){
+            string1=string1+topb1;
+        }else{
+            string1=string1+drawLine(19)
+        }
+    return string1;
     }
-        console.log(drawLine(19));
+}
+    console.log(drawtopborder(19));
+//function to drawmissdle border
+function drawmiddleborder(a){
     
+}
