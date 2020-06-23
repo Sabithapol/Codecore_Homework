f72171f100da147f26ad032e103ef1424b520c81
Sabithapol <Sabitha.poladi10@gmail.com>
2020-06-22 20:22:51 -0700

    fff

--- javaobjects/Array1.js
+++
@@ -1,6 +0,0 @@
-function toArray(obj){
-   newarray = [];
-   for (let key in obj){
-        newarray.push([key,obj[key]])
-    } 
-}console.log(toArray({a: 1, b: 2, c:3}))
--- javaobjects/mergeobjects.js
+++
@@ -1,3 +0,0 @@
-function merge(objA,objB){
-    
-}
